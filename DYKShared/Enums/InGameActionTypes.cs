9c7fdc94ee1439eb83f059671897b7c9abb47f45
Equelister <daczekpl@gmail.com>
2022-01-02 00:22:38 +0100

    Added Actions and View

---
+++ DYKShared/Enums/InGameActionTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DYKShared.Enums
+{
+    public enum InGameActionTypes
+    {
+        SwitchFirstWithLastLetterQuestions,
+        SwitchFirstWithLastLetterAnswers,
+        DeleteSomeLettersQuestions,
+        DeleteSomeLettersAnswers,
+        FloatingAnswers,
+        HitIt5TimesAnswers,
+        DisplayOnlyOnHoverAnswers
+    }
+}
---
+++ DYKShared/Model/InGameActions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DYKShared.Model
+{
+    public class InGameActions
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public InGameActions()
+        {
+        }
+
+        private InGameActions(int id, string name, string desc)
+        {
+            ID = id;
+            Name = name;
+            Description = desc;
+        }
+
+        public static List<InGameActions> GenerateActions()
+        {
+            List<InGameActions> list = new List<InGameActions>();
+            list.Add(new InGameActions(1, "Question Switcher", "Switches first letter with last in every word in a question."));
+            list.Add(new InGameActions(2, "Answer Switcher", "Switches first letter with last in every word in each answer."));
+            list.Add(new InGameActions(3, "Question Remover", "Deletes 15% of letters in a question."));
+            list.Add(new InGameActions(4, "Answer Remover", "Deletes 15% of letters in each answer."));
+            list.Add(new InGameActions(5, "Multi-Clicker", "You need to click 5 times in order to select your answer."));
+            list.Add(new InGameActions(6, "Flashlight", "Shows answer only if you have your mouse cursor on it."));
+            return list;
+        }
+
+        public static List<InGameActions> GetRandomAmoutOfActions(List<InGameActions> actionList ,int userCount)
+        {
+            List<InGameActions> resultList = new List<InGameActions>();
+            Random rand = new Random();
+            int numberOfActionsPerUser = (int)Math.Ceiling((userCount * (0.24)) > 1 ? (userCount * (0.24) + 0.1) : (userCount * (0.24))); // 1 action for 2-4 users, 2 for 5-7, 3 for 8
+            for(int i = 0; i<numberOfActionsPerUser; i++)
+            {
+                int random = rand.Next(actionList.Count);
+                if (resultList.Contains(actionList.ElementAt(random)))
+                {
+                    i--;
+                }
+                else
+                {
+                    resultList.Add(actionList.ElementAt(random));
+                }
+            }
+            return resultList;
+        }
+    }
+
+}
